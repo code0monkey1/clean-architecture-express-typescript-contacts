d9f444af7d67fa8e9e920c576654ea7ed43a4e83
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-07 15:37:11 +0000

    7 Sept 2023, 21:07

--- src/domain/interfaces/repositories/contact-repository.ts
+++ src/domain/interfaces/repositories/contact-repository.ts
@@ -1,10 +1,13 @@
 // domain/interfaces/repositories/contact-repository.ts
-import { ContactRequestModel, ContactResponseModel } from "../../models/contact";
+import {
+  ContactRequestModel,
+  ContactResponseModel,
+} from '../../models/contact';
 
 export interface ContactRepository {
-    createContact(contact: ContactRequestModel): void;
-    deleteContact(id: String): void;
-    updateContact(id: String, data: ContactRequestModel): void;
-    getContacts(): Promise<ContactResponseModel[]>;
-    getContact(id: String): Promise<ContactResponseModel | null>;
+  createContact(contact: ContactRequestModel): void;
+  deleteContact(id: String): void;
+  updateContact(id: String, data: ContactRequestModel): void;
+  getContacts(): Promise<ContactResponseModel[]>;
+  getContact(id: String): Promise<ContactResponseModel | null>;
 }
