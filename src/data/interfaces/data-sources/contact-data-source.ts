53f4777002ef86b03dd935fca416dac4a783243a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-13 11:46:27 +0000

    13 Sept 2023, 17:16

--- src/data/interfaces/data-sources/contact-data-source.ts
+++ src/data/interfaces/data-sources/contact-data-source.ts
@@ -1,10 +1,13 @@
 // data/data-sources/contact-data-source.ts
-import { ContactRequestModel, ContactResponseModel } from "../../../domain/models/contact";
+import {
+  ContactRequestModel,
+  ContactResponseModel,
+} from '../../../domain/models/contact';
 
 export interface ContactDataSource {
-    create(contact: ContactRequestModel): void;
-    getAll(): Promise<ContactResponseModel[]>;
-    deleteOne(id: String): void;
-    updateOne(id: String, data: ContactRequestModel): void;
-    getOne(id: String): Promise<ContactResponseModel | null>;
+  create(contact: ContactRequestModel): void;
+  getAll(): Promise<ContactResponseModel[]>;
+  deleteOne(id: String): void;
+  updateOne(id: String, data: ContactRequestModel): void;
+  getOne(id: String): Promise<ContactResponseModel | null>;
 }
