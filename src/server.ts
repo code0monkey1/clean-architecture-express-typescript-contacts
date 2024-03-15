8d69d20f0248a914ad477bc8c8f5f9a042045c3b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-03-15 19:04:37 +0000

    16 Mar 2024, 00:34

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import logger from "./config/logger";
 const server = express();
+
 server.use(express.json());
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  logger.info("server listening on port ", PORT);
+});
+
 export default server;
