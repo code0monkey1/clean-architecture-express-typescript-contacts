b0df77594179a19b647c40f5d4d2ea9b4d0e0881
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-03-15 18:57:01 +0000

    16 Mar 2024, 00:27

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,3 @@
-import { MongoClient } from "mongodb";
-import { Pool } from "pg";
-import { MongoDBContactDataSource } from "./data/data-sources/mongodb/mongodb-contact-data-source";
-import { PGContactDataSource } from "./data/data-sources/postgresql/pg-contact-data-source";
-import { NoSQLDatabaseWrapper } from "./data/interfaces/data-sources/nosql-database-wrapper";
-
 import express from "express";
 
 const app = express();
